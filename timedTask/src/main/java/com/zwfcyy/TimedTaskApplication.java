4a5e2efd07bbd7d6593d1da8afe79b479b87ccb2
zwf <echo.zwf@outlook.com>
2024-01-15 09:05:06 +0800

    no message

---
+++ timedTask/src/main/java/com/zwfcyy/TimedTaskApplication.java
@@ -0,0 +1,13 @@
+package com.zwfcyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimedTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimedTaskApplication.class, args);
+    }
+
+}

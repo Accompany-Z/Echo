118767ada145cf6e54c9a05a7dee804eba209ec9
zwf <echo.zwf@outlook.com>
2024-04-11 18:13:55 +0800

    no message

--- echo-vue/src/views/dashboard/Analysis/data.ts
+++ echo-vue/src/views/dashboard/Analysis/data.ts
@@ -10,7 +10,7 @@ export interface GrowCardItem {
 
 export const growCardList: GrowCardItem[] = [
   {
-    title: '访问数',
+    title: '材料入库',
     icon: 'visit-count|svg',
     value: 2000,
     total: 120000,
@@ -18,7 +18,7 @@ export const growCardList: GrowCardItem[] = [
     action: '月',
   },
   {
-    title: '成交额',
+    title: '材料出库',
     icon: 'total-sales|svg',
     value: 20000,
     total: 500000,
@@ -26,20 +26,20 @@ export const growCardList: GrowCardItem[] = [
     action: '月',
   },
   {
-    title: '下载数',
+    title: '毛坯入库',
     icon: 'download-count|svg',
     value: 8000,
     total: 120000,
     color: 'orange',
-    action: '周',
+    action: '月',
   },
   {
-    title: '成交数',
+    title: '出货量',
     icon: 'transaction|svg',
     value: 5000,
     total: 50000,
     color: 'purple',
-    action: '年',
+    action: '月',
   },
 ];
 
